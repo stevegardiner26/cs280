1bff34eeee2fc04ebb4acecebd8ebda68ef31ada
Steve <stevegardiner26@aol.com>
2019-09-09 09:17:49 -0400

    Updated Project Paths

---
+++ Projects/hello_world.cpp
@@ -0,0 +1,12 @@
+//
+// Created by Steve Gardiner on 9/9/19.
+//
+#include <iostream>
+using namespace std;
+
+// Main Function
+int hello_world() {
+    cout << "Hello World" << endl;
+    return 0;
+}
+
--- main.cpp
+++ main.cpp
@@ -1,7 +1,10 @@
 #include <iostream>
 using namespace std;
 
+#include "Projects/hello_world.cpp"
+
 int main() {
-    cout << "Hello, World!" << endl;
+    // cout << "Hello, World!" << endl;
+    hello_world();
     return 0;
 }
