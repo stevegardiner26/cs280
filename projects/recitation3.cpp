7d6c5762f45d847f29c42a7f310ada7e313183a4
Penguin Steve <stevegardiner26@aol.com>
2019-09-23 11:32:51 -0400

    Renamed Main Func in Recitation 3 File

--- projects/recitation3.cpp
+++ projects/recitation3.cpp
@@ -4,7 +4,7 @@
 #include <iostream>
 using namespace std;
 
-int main() {
+int recitation3() {
     char letters[26] = {'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'};
     char selected[26];
     srand((unsigned)time(0));
